0a57d7b842463c457e7e4cb4a8314860735ba294
Amelie1987 <102588943+Amelie1987@users.noreply.github.com>
2023-06-16 10:20:39 +0200

    add JSON stuff

--- src/main/java/at/fhtw/swen2/jsonConverter/JSONConverter.java
+++ src/main/java/at/fhtw/swen2/jsonConverter/JSONConverter.java
@@ -3,6 +3,8 @@ package at.fhtw.swen2.jsonConverter;
 import at.fhtw.swen2.controller.TourController;
 import at.fhtw.swen2.model.TourDTO;
 import at.fhtw.swen2.model.TourLogDTO;
+import at.fhtw.swen2.presentation.viewmodel.TourListViewModel;
+import at.fhtw.swen2.presentation.viewmodel.TourViewModel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.scene.control.Alert;
@@ -13,11 +15,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 public class JSONConverter {
-    TourController tourController = new TourController();
+    @Autowired
+    TourViewModel tourViewModel;
+    @Autowired
+    TourListViewModel tourListViewModel;
     public void writeJSONFile(String tours, String tourLogs) throws IOException {
         // Get the current timestamp
         LocalDateTime currentTime = LocalDateTime.now();
@@ -26,8 +34,8 @@ public class JSONConverter {
       //  String filename =
         FileWriter file = new FileWriter("src/main/java/at/fhtw/swen2/jsonConverter/JSONFiles/tours_" + timestamp + ".json");
         file.write(tours);
-        file.write("\n");
-        file.write(tourLogs);
+      //  file.write("\n");
+       // file.write(tourLogs);
         file.close();
         //user feedback
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -39,7 +47,7 @@ public class JSONConverter {
         //
     public void importJSONFile(String filename) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        filename = "src/main/java/at/fhtw/swen2/jsonConverter/JSONFiles/tours_2023-06-14_15-23-17.json";
+        filename = "src/main/java/at/fhtw/swen2/jsonConverter/JSONFiles/tours_2023-06-16_09-44-11.json";
         BufferedReader reader;
         String tourLogs = "";
         String tours = "";
@@ -47,34 +55,26 @@ public class JSONConverter {
             reader = new BufferedReader(new FileReader(filename));
             tours = reader.readLine();
             //System.out.println("Readed: " + tours);
-            while (tours != null) {
-                System.out.println(tours);
-                // read next line = tourLogs
-                tourLogs = reader.readLine();
-            }
+
 
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
-//JSON from file to Object
-            List<TourDTO> tourList = Collections.singletonList(mapper.readValue(tours, TourDTO.class));
-          // needed?  List <TourLogDTO> tourLogList = Collections.singletonList(mapper.readValue(tourLogs, TourLogDTO.class));
-
-           // System.out.println(tourList);
+            //JSON from JSON String to array of tourDTOs
+            List<TourDTO> tourList = Arrays.asList(mapper.readValue(tours, TourDTO[].class));
+            System.out.println("tours from json file" + tours);
             // post tour by tour
             for (int i = 0; i < tourList.size(); i++) {
 
-
+                TourDTO tour = tourList.get(i);
                 //System.out.println(tourList.get(i));
-                tourController.createTour(tourList.get(i));
+                tourViewModel = new TourViewModel(tour);
+                tourViewModel.createTour();
             }
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-
-//JSON from String to Object
-        // = mapper.readValue(tourLogs, TourLogDTO.class)
     }
 }
--- src/main/java/at/fhtw/swen2/service/SearchService.java
+++
@@ -1,8 +0,0 @@
-package at.fhtw.swen2.service;
-
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SearchService {
-}
